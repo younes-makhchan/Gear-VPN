35c1763bff960cf3f17fe1da2c7d327889f4bab0
anaserrami <anaserrami24@gmail.com>
2024-05-06 15:42:23 +0100

    change colors

--- app/src/main/java/com/kpstv/vpn/ui/theme/Color.kt
+++ app/src/main/java/com/kpstv/vpn/ui/theme/Color.kt
@@ -2,31 +2,31 @@ package com.kpstv.vpn.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFFFFFFF)
-val Purple500 = Color(0xFFFFFFFF)
-val Purple700 = Color(0xFFFFFFFF)
-val Teal200 = Color(0xFFFFFFFF)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
 
-val foregroundColor = Color(0xFFFFFFFF)
-val secondaryTextColorDark = Color(0xFFFFFFFF)
-val secondaryTextColorMoreDark = Color(0xFFFFFFFF)
+val foregroundColor = Color(0xFF000000)
+val secondaryTextColorDark = Color(0xFF000000)
+val secondaryTextColorMoreDark = Color(0xFF627199)
 
-val dotColor = Color(0xFFFFFFFF)
-val highlightColor = Color(0xFFFFFFFF)
+val dotColor = Color(0xff004aad)
+val highlightColor = Color(0xA1EDAB0A)
 
 val background = Color(0xFFFFFFFF)
-val backgroundMiddle = Color(0xFFFFFFFF)
-val backgroundInner = Color(0xFFFFFFFF)
-val goldenYellow = Color(0xFFFFFFFF)
-val goldenYellowDark = Color(0xFFFFFFFF)
-val goldenYellowBright = Color(0xFFFFFFFF)
-val cyan = Color(0xFFFFFFFF)
+val backgroundMiddle = Color(0xFF162B53)
+val backgroundInner = Color(0xFF182E58)
+val goldenYellow = Color(0xFFCBAF1C)
+val goldenYellowDark = Color(0xFF817017)
+val goldenYellowBright = Color(0xFFEDAB0A)
+val cyan = Color(0xFF4FCFE7)
 
-val greenColor = Color(0xFFFFFFFF)
-val greenColorDark = Color(0xFFFFFFFF)
+val greenColor = Color(0xFF38C05A)
+val greenColorDark = Color(0xFF38916C)
 
-val cyanDark = Color(0xFFFFFFFF)
-val purpleColor = Color(0xFFFFFFFF)
+val cyanDark = Color(0xFF40A5B8)
+val purpleColor = Color(0xFF882391)
 
-val primaryColor = Color(0xFFFFFFFF)
-val primaryVariantColor = Color(0xFFFFFFFF)
+val primaryColor = Color(0xff38b6ff)
+val primaryVariantColor = Color(0xff38b6ff)
